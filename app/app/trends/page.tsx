74f140e558e356087477afc842fb102ade10058f
Anthony Appiah Adjei <adjeianthony77@gmail.com>
2026-08-25 17:45:26 +0000

    Created routes for the various app components

---
+++ app/app/trends/page.tsx
@@ -0,0 +1,8 @@
+export default function TrendsPage() {
+    return (
+        <>
+            <h1>Trends</h1>
+            <p>View the latest trends and insights through our comprehensive visualization tools.   </p>
+        </>
+    )
+}
